206c73620938f0a8785d726c8b5ac2ff976eb8cf
Jerd <inquisitivequin@gmail.com>
2018-02-04 12:58:05 -0800

    4-Feb

--- Tea.java
+++ Tea.java
@@ -27,38 +27,86 @@ public static Integer findStopCodon(String dna,
 }
 
 	public static void testDna() {
-		System.out.println(findStopCodon("atgsdcgdfdssadsadsadsadsadsadstaa", 0,"taa"));
+		//
+		System.out.println(findStopCodon("asdasdatgasdasdasdasdtaa", 0,"taa"));
+		System.out.println(findStopCodon("atgasdasdasdasdtag", 0,"tag"));
+		System.out.println(findStopCodon("asdasatgasdasdtga", 0,"tga"));
 	}
 
-/*
+//---------------------------------------------------------------------------->
 
-3. Write the method findStopCodon that has three parameters, a String parameter named dna, an integer parameter named startIndex that represents where the first occurrence of ATG occurs in dna, and a String parameter named stopCodon. This method returns the index of the first occurrence of stopCodon that appears past startIndex and is a multiple of 3 away from startIndex. If there is no such stopCodon, this method returns the length of the dna strand.
 
-4. Write the void method testFindStopCodon that calls the method findStopCodon with several examples and prints out the results to check if findStopCodon is working correctly. Think about what types of examples you should check. For example, you may want to check some strings of DNA that have genes and some that do not. What other examples should you check?
-
-5. Write the method findGene that has one String parameter dna, representing a string of DNA. In this method you should do the following:
-*/
-
-public static void findGene(String dna) {
-	Integer startInd = dna.indexOf("ATG");
+public static String findGene(String dna, int start) {
+		dna = dna.toLowerCase();
+		int startInd = dna.indexOf("atg", start);;
 		if(startInd == -1) {
 			return "";
 		};
+		int taaInd = findStopCodon(dna, startInd, "taa");
+		int tagInd = findStopCodon(dna, startInd, "tag");
+		int tgaInd = findStopCodon(dna, startInd, "tga");
+		int minInd = 0;
+		if (taaInd == -1 ||
+					(tgaInd != -1 && tgaInd < taaInd)) {
+			minInd = tgaInd;
+		} else {
+			minInd = taaInd;
+		}
+		return dna.substring(startInd, minInd + 3);
 };
 
-/*
-Find the index of the first occurrence of the start codon “ATG”. If there is no “ATG”, return the empty string.
-Find the index of the first occurrence of the stop codon “TAA” after the first occurrence of “ATG” that is a multiple of three away from the “ATG”. Hint: call findStopCodon.
-Find the index of the first occurrence of the stop codon “TAG” after the first occurrence of “ATG” that is a multiple of three away from the “ATG”. Find the index of the first occurrence of the stop codon “TGA” after the first occurrence of “ATG” that is a multiple of three away from the “ATG”.
-Return the gene formed from the “ATG” and the closest stop codon that is a multiple of three away. If there is no valid stop codon and therefore no gene, return the empty string.
-6. Write the void method testFindGene that has no parameters. You should create five DNA strings. The strings should have specific test cases such as DNA with no “ATG”, DNA with “ATG” and one valid stop codon, DNA with “ATG” and multiple valid stop codons, DNA with “ATG” and no valid stop codons. Think carefully about what would be good examples to test. For each DNA string you should:
+public static void testFindGene() {
+	String dna1 = "ATGGTATAGAGTATAA";
+	//System.out.println(findGene(dna1));
+}
 
-Print the DNA string.
-Calculate the gene by sending this DNA string as an argument to findGene. If a gene exists following our algorithm above, then print the gene, otherwise print the empty string.
-7. Write the void method printAllGenes that has one String parameter dna, representing a string of DNA. In this method you should repeatedly find genes and print each one until there are no more genes. Hint: remember you learned a while(true) loop and break 
+public static void printAllGenes(String dna) {
+	dna = dna.toLowerCase();
+	int start = 0;
+	while(true) {
+		String curGene = findGene(dna, start);
+
+		if(curGene.isEmpty()) {
+			break;
+		}
+		System.out.println(curGene);
+
+		start = dna.indexOf(curGene, start) + curGene.length();
+	}
+
+}
+
+public static Integer howMany(String fnd, String here) {
+		int total = 0;
+		int currInd = here.indexOf(fnd);
+	while(currInd != -1) {
+		if(currInd != -1) {
+			return total = total + 1;
+		} else {
+			currInd = here.indexOf(fnd, currInd + 1);
+		}
+	}
+	return total;
+}
+
+/*
+2. Write the method named howMany that has two String parameters named stringa
+and stringb. This method returns an integer indicating how many times stringa
+appears in stringb, where each occurrence of stringa must not overlap with
+another occurrence of it. For example, the call howMany(“GAA”,
+“ATGAACGAATTGAATC”) returns 3 as GAA occurs 3 times. The call howMany(“AA”,
+“ATAAAA”) returns 2. Note that the AA’s found cannot overlap.
+
+3.Write the void method testHowMany has no parameters. Add code in here to
+call howMany with several examples and print the results. Think carefully
+about what types of examples would be good to test to make sure your method
+works correctly.
 */
 
 	public static void main (String[] args) {
 			testDna();
+			//testFindGene();
+			//printAllGenes("asdasdatgasdasdasdasdtaaatgaggaggtagatatatgaggaggtga");
+			System.out.println(howMany("gaa","atgaacgaattgaatc"));
 	}
 }
